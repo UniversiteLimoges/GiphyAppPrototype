da626c38699ff72a22b3815f17049b16a19da351
Quentin <solid@Solid-Peace.aircard>
2020-03-25 00:22:49 +0100

    going to modify user name via new methode

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class TestController extends Controller
 {
-    public function viewTest() 
+
+	public $name = "quentin";
+
+	public $users_name = [];
+
+    public function viewTest()
     {
-    	return view('test');
+
+    	$users = User::all();  // return Collection
+    	
+    	foreach ($users as $user) {
+    		array_push($this->users_name, $user->name);
+    	}
+
+
+    	return view('test', [
+    		'name' => $this->name,
+    		'names' => $this->users_name,
+    	]);
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,4 +2,19 @@
 
 @section('content')
 	<h1>hello test user</h1>
+	
+	<p>My name is {{ $name }}</p>
+
+	<hr>
+
+	<p>Liste des utilisateurs</p>
+
+	<ul>
+		@foreach ($names as $name)
+			<li>{{ $name }}</li>
+		@endforeach
+	</ul>
+
+
+
 @endsection
