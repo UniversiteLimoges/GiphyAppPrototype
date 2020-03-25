6e037635a2533bc3388624e30812fe2201332294
GDK21 <g.denysiak21@gmail.com>
2020-03-25 11:13:09 +0100

    WIP: navigation

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav class="navigation">
+    <div class="navigation__back">
+        <i class="chevron-circle-left"></i>
+    </div>
+    <div class="navigation__home">
+        <i class="home"></i>
+    </div>
+</nav>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="../js/index.js"></script>
-    <title>React Laravel Boilerplate</title>
+@include('components/head')
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app">
+        blabla
+    </div>
+    <div id="nav">
+        @include('components/navigation')
+    </div>
 </body>
 </html>
